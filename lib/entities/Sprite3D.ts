f22a1a3f6702c31dea8c7558b681545f3347a45f
80prozent <80prozent@differentdesign.de>
2017-05-04 21:42:44 +0200

    minor update to Sprite3d dummy

--- lib/entities/Sprite3D.ts
+++ lib/entities/Sprite3D.ts
@@ -1,8 +1,28 @@
-// should be adapter for billboard
+
 import {Billboard} from "@awayjs/scene"
 import {Entity} from "./Entity"
+import {MaterialBase} from "../materials/MaterialBase";
+
 export class Sprite3D extends Entity{
+
+	private _material:MaterialBase;
+
 	constructor(mat:any, param1:number, param2:number){
 		super();
+	}
+	public get material():MaterialBase
+	{
+		return this._material;
+	}
+
+	public set material(value:MaterialBase)
+	{
+		if (value == this._material)
+			return;
+		//if (this._material)
+		//	this._material.removeOwner(this);
+		//this._material = value;
+		//if (this._material)
+		//	this._material.addOwner(this);
 	}
 }
