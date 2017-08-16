707cd457ded4e2b073b4221543717ce20baec613
80prozent <80prozent@differentdesign.de>
2017-08-17 01:15:43 +0200

    add stub for MeshHelper.invertFaces

--- lib/tools/helpers/MeshHelper.ts
+++ lib/tools/helpers/MeshHelper.ts
@@ -1,1 +1,9 @@
-export class MeshHelper {}
+
+import {Mesh} from "../../entities/Mesh"
+export class MeshHelper {
+
+	public static invertFaces(target:Mesh){
+		console.log("invert Faces is only a stub");
+	}
+
+}
