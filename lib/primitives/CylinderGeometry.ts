64893cc54131b43e5f32d1b6acbbdd0b3e61edb4
80prozent <80prozent@differentdesign.de>
2017-08-31 14:20:36 +0200

    fixes for CyclinerPrimitive

--- lib/primitives/CylinderGeometry.ts
+++ lib/primitives/CylinderGeometry.ts
@@ -9,4 +9,60 @@ export class CylinderGeometry extends PrimitiveBase
 	{
 		super((prefabTopRadius instanceof PrimitiveCylinderPrefab)? prefabTopRadius : new PrimitiveCylinderPrefab(null, "triangle", prefabTopRadius, bottomRadius, height, segmentsW, segmentsH, topClosed, bottomClosed, surfaceClosed, yUp));
 	}
+
+	public get yUp():boolean{
+		return (<PrimitiveCylinderPrefab>this._prefab).yUp;
+	}
+	public set yUp(value:boolean){
+		(<PrimitiveCylinderPrefab>this._prefab).yUp=value;
+	}
+	
+	public get topClosed():boolean{
+		return (<PrimitiveCylinderPrefab>this._prefab).topClosed;
+	}
+	public set topClosed(value:boolean){
+		(<PrimitiveCylinderPrefab>this._prefab).topClosed=value;
+	}
+	
+	public get bottomClosed():boolean{
+		return (<PrimitiveCylinderPrefab>this._prefab).bottomClosed;
+	}
+	public set bottomClosed(value:boolean){
+		(<PrimitiveCylinderPrefab>this._prefab).bottomClosed=value;
+	}
+	
+	public get segmentsW():number{
+		return (<PrimitiveCylinderPrefab>this._prefab).segmentsW;
+	}
+	public set segmentsW(value:number){
+		(<PrimitiveCylinderPrefab>this._prefab).segmentsW=value;
+	}
+	
+	public get segmentsH():number{
+		return (<PrimitiveCylinderPrefab>this._prefab).segmentsH;
+	}
+	public set segmentsH(value:number){
+		(<PrimitiveCylinderPrefab>this._prefab).segmentsH=value;
+	}
+	
+	public get bottomRadius():number{
+		return (<PrimitiveCylinderPrefab>this._prefab).bottomRadius;
+	}
+	public set bottomRadius(value:number){
+		(<PrimitiveCylinderPrefab>this._prefab).bottomRadius=value;
+	}
+	
+	public get topRadius():number{
+		return (<PrimitiveCylinderPrefab>this._prefab).topRadius;
+	}
+	public set topRadius(value:number){
+		(<PrimitiveCylinderPrefab>this._prefab).topRadius=value;
+	}
+	
+	public get height():number{
+		return (<PrimitiveCylinderPrefab>this._prefab).height;
+	}
+	public set height(value:number){
+		(<PrimitiveCylinderPrefab>this._prefab).height=value;
+	}
 }
